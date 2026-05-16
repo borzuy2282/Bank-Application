eabe86fc1a44808f857bd7e8a07dbe0d593eb64a
Tymofii <tim.barzashvili@icloud.com>
2026-05-16 11:18:32 +0200

    Account entity and repository
    Created account entity and repository, table in database was also created

---
+++ src/main/java/com/springboot/bankapplication/entity/Account.java
@@ -0,0 +1,29 @@
+package com.springboot.bankapplication.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "account")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "holder_name")
+    private String holderName;
+    private double balance;
+}
