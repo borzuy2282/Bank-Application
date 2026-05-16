d0da3f336d3ea692ce5dfd5c84d7ee729e8d8c51
Tymofii <tim.barzashvili@icloud.com>
2026-05-16 15:00:02 +0200

    Transaction entity
    Added transaction entity for table transaction that stores the transaction. For now it is not directly related to Account table, will be fixed after implementing. TransactionType enum was created as well for easier usage of all possible types of transaction

---
+++ src/main/java/com/springboot/bankapplication/entity/Transaction.java
@@ -0,0 +1,29 @@
+package com.springboot.bankapplication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Table(name = "transaction")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long accountId;
+    private TransactionType type;
+    private double amount;
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/com/springboot/bankapplication/entity/TransactionType.java
@@ -0,0 +1,8 @@
+package com.springboot.bankapplication.entity;
+
+public enum TransactionType {
+    DEPOSIT,
+    WITHDRAW,
+    INCOMING_TRANSFER,
+    OUTCOMING_TRANSFER
+}
